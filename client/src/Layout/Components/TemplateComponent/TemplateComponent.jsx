a47cfb52381c69336b55a7fc8a5d41c80e1d3bbf
Duc Nguyen <mingduc17082002@gmail.com>
2023-05-12 18:05:59 +0700

    Build Banner Template

--- client/src/Layout/Components/TemplateComponent/TemplateComponent.jsx
+++ client/src/Layout/Components/TemplateComponent/TemplateComponent.jsx
@@ -6,28 +6,47 @@ function TemplateComponent() {
     const videoList = [
         {
             id: 1,
-            options: {
-                text: 2,
-                api: '/api/video/storyVideo1',
-            },
-            url: 'https://creatomate-static.s3.amazonaws.com/demo/video1.mp4',
-        },
-
-        {
-            id: 2,
             url: 'https://cdn.creatomate.com/renders/aad516eb-271c-4cd0-87da-552f5cc8a189.mp4',
             options: {
                 image: 1,
                 text: 2,
-                api: '/api/video/storyVideo1',
+                api: '/video/templateVideo1',
+            },
+        },
+        {
+            id: 2,
+            url: 'https://cdn.creatomate.com/renders/49b37602-5d98-47bd-9771-5b11f7a2c983.mp4',
+            options: {
+                image: 1,
+                text: 2,
+                api: '/video/templateVideo2',
             },
         },
         {
             id: 3,
-            url: 'https://creatomate-static.s3.amazonaws.com/demo/video3.mp4',
+            url: 'https://cdn.creatomate.com/renders/26ab65a3-5133-4081-b0dc-56d9da97d97c.mp4',
             options: {
+                image: 2,
+                text: 1,
+                api: '/video/templateVideo3',
+            },
+        },
+        {
+            id: 4,
+            url: 'https://cdn.creatomate.com/renders/30f2ebe4-9ea4-41fc-93f8-2d917c4e0e2d.mp4',
+            options: {
+                image: 1,
+                text: 3,
+                api: '/video/templateVideo4',
+            },
+        },
+        {
+            id: 5,
+            url: 'https://cdn.creatomate.com/renders/9b295a9d-3cb2-4978-8e65-756d97a15770.mp4',
+            options: {
+                image: 1,
                 text: 2,
-                api: '/api/video/storyVideo1',
+                api: '/video/templateVideo5',
             },
         },
     ];
@@ -54,7 +73,7 @@ function TemplateComponent() {
                     <div className="template-title-container">
                         <h1 className="template-title">Templates</h1>
                     </div>
-                    <h2 className="template-category">Messages</h2>
+                    <h2 className="template-category">Banners</h2>
                     <div className="stock-videos-list">
                         {videoList.map((video) => (
                             <video
